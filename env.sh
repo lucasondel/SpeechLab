ddbfaf8c37b2ef0f5a60e0526423eabab9ff2c90
Lucas Ondel <lucas.ondel@gmail.com>
2021-06-04 12:52:17 +0200

    Setup environment script

---
+++ env.sh
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: MIT
+
+# Global environment variable for SpeechLab. We recommend the following
+# directories to be set on hard drive with large memory as they will
+# serve to store features, models, etc.
+
+SLAB_ROOT=~/SpeechLab
+SLAB_DATASETS=~/Datasets
+SLAB_FEATURES=~/Features
