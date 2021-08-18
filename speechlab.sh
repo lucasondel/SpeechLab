58160b1d5786f05cf2a316c6d522895627b03a6b
Lucas Ondel <lucas.ondel@gmail.com>
2021-08-18 14:01:34 +0200

    Added lfmmi to the path

--- speechlab.sh
+++ speechlab.sh
@@ -4,5 +4,6 @@
 
 export PATH=$SLAB_ROOT/tools/features:$PATH
 export PATH=$SLAB_ROOT/tools/gmm:$PATH
+export PATH=$SLAB_ROOT/tools/lfmmi:$PATH
 export PATH=$SLAB_ROOT/tools/monophone:$PATH
 export PATH=$SLAB_ROOT/tools/utils:$PATH
